c758baf70734f0128a912eb2a68598267274ccad
Nilsa Nhatsave <nilsanhatsave2@gmail.com>
2026-07-17 23:25:39 +0200

    remove: pastas e arquivos duplicados (- Copy)

--- pages - Copy/__init__.py
+++
@@ -1,8 +0,0 @@
-# pages/__init__.py
-from . import login
-from . import home
-from . import enfermeiro
-from . import medico
-from . import nutricionista
-from . import agronomo
-from . import admin
